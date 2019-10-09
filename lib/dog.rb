d298490d0f9b8f91cdc68be01ae3e3f685445a4c
Charity Parks <tuppercharity@gmail.com>
2019-10-09 20:59:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,23 @@
 class Dog
+
+@@all = []
   
+attr_accessor :owner :mood
+attr_reader :name
+
+  def initialize(name, owner, mood = "nervous")
+    @name = name
+    @owner = owner
+    @mood = mood
+    @@all << self
+    save
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end 
 end
