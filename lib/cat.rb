94844bcf2b8fb64af778c5356bc2c92d4aa7f28e
Charity Parks <tuppercharity@gmail.com>
2019-10-09 20:55:19 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -7,9 +7,10 @@ end
 attr_accessor :owner :mood
 attr_reader :name
 
-  def initialize(name, owner)
+  def initialize(name, owner, mood = "nervous")
     @name = name
     @owner = owner
+    @mood 
     save
   end
   
