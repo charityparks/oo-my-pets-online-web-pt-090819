03ee7500e6ba2c6c2e77fd8e98c550898053735f
Charity Parks <tuppercharity@gmail.com>
2019-10-09 20:28:30 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,20 +1,22 @@
 class Cat
-
-@@all = []
   
-attr_accessor :name
-
-  def initialize(name, owner)
-    @name = name
-    @owner = owner
-    save
-  end
-  
-  def save
-    @@all << self
-  end
-  
-  def self.all
-    @@all
-  end
 end
+
+# @@all = []
+  
+# attr_accessor :name
+
+#   def initialize(name, owner)
+#     @name = name
+#     @owner = owner
+#     save
+#   end
+  
+#   def save
+#     @@all << self
+#   end
+  
+#   def self.all
+#     @@all
+#   end
+# end
